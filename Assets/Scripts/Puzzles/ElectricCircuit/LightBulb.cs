316a0d155a1e7a25f85db37ebc9681789aa1befc
Xiaohan Wu <wuxiaohan.a@gmail.com>
2024-06-20 00:03:10 -0400

    finished basic workflow of circuit; need to implement gamecontroller player lock and unlock, and visual effects

---
+++ Assets/Scripts/Puzzles/ElectricCircuit/LightBulb.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBulb : MonoBehaviour
+{
+    public void LightOn()
+    {
+        Debug.Log("light on");
+    }
+
+    public void LightOff()
+    {
+        Debug.Log("light off");
+    }
+}
